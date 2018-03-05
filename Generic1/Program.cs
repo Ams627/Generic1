72dae6fc2964473a383b69a4691355aa589288ba
Adrian Sims <adriansims62781@gmail.com>
2018-03-05 21:09:00 +0000

    Add project files.

---
+++ Generic1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic1
+{
+    class X
+    {
+        
+    }
+    internal class Program
+    {
+        static int P() { return 12334;}
+        private static Func<object, object> R(Type t)
+        {
+            Func<int> f = P;
+            var o = (object) f;
+            var gentype = typeof(Func<,>);
+            var typeargs = new[] {t, typeof(object)};
+            var constructed = gentype.MakeGenericType(typeargs);
+            var result = (Func<object, object>)Activator.CreateInstance(constructed, (Func<int>)P);
+            return result;
+        }
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var result = R(typeof(X));
+                Console.WriteLine();
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add Generic1/Program.cs
+git add Generic1/App.config
+git add Generic1/Properties/AssemblyInfo.cs
+git add Generic1/Generic1.csproj
+git add Generic1.sln
+git add .gitignore
