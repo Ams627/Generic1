39a1469fa4a077251fcb8bde8871e1ab054fa356
Adrian Sims <adriansims62781@gmail.com>
2018-03-06 07:13:45 +0000

    Changes.

--- Generic1/Program.cs
+++ Generic1/Program.cs
@@ -14,14 +14,17 @@ namespace Generic1
     internal class Program
     {
         static int P() { return 12334;}
+        int P2() { return 12334; }
         private static Func<object, object> R(Type t)
         {
             Func<int> f = P;
             var o = (object) f;
+            Func<int> f2 = new Program().P2;
+
             var gentype = typeof(Func<,>);
             var typeargs = new[] {t, typeof(object)};
             var constructed = gentype.MakeGenericType(typeargs);
-            var result = (Func<object, object>)Activator.CreateInstance(constructed, (Func<int>)P);
+            var result = (Func<object, object>)Activator.CreateInstance(constructed, P);
             return result;
         }
         private static void Main(string[] args)
